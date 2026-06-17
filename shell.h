d4639f2bd8c91f1edb21ea653a1b80800589dd65
Caitlin Corpus <corpus0629@gmail.com>
2026-06-17 11:50:46 +0900

    Add pipeCommand function

--- shell.h
+++ shell.h
@@ -2,9 +2,11 @@
 #define SHELL_H
 
 #include <string>
+#include <vector>
 
 void displayPrompt();
 bool handleCommand(std::string input);
+void pipeCommand(std::vector<std::vector<std::string>> commands);
 
 #endif
 
