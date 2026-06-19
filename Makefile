16f94154e56bc24a263d32cdfc14dccf26c08064
Angela Lee <angelashinyoung@gmail.com>
2026-06-18 20:15:34 -0700

    Final Assignment 2 submission

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@
 # Programming Assignment #2 - XSH
 # Angela Lee (cssc3130)
 # Caitlin Corpus (cssc3114)
+# Filename: Makefile
 # ###########################################################
 
 EXEC = xsh
--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
  * CS480 Summer 2026
  * Programming Assignment #2 - XSH
  * Angela Lee (cssc3130)
- * Caitlin Corpus (cssc3114)      
+ * Caitlin Corpus (cssc3114)  
+ * Filename: main.cpp    
  */
 
 #include <iostream>
